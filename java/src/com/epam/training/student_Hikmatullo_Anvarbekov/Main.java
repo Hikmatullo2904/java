fa3f1d766759eee65ba85c925e251931bad27ed2
Hikmatullo2904 <hikmatulloanvarbekov001@gmail.com>
2023-04-23 13:10:13 +0500

    I added few lines

--- java/src/com/epam/training/student_Hikmatullo_Anvarbekov/Main.java
+++ java/src/com/epam/training/student_Hikmatullo_Anvarbekov/Main.java
@@ -1,7 +1,13 @@
 package com.epam.training.student_Hikmatullo_Anvarbekov;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Hello world!");
+        System.out.println("what the fuck are you doing?");
+        String question = sc.nextLine();
+        System.out.println(question + " => " + " mind your own business.");
     }
 }
